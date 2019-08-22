b6ffc809ed0034ece0408301c9a92a358178caf9
nicolasB-student <nicolas.blanchecotte@imie.fr>
2019-08-22 14:33:12 +0200

    test

--- app/src/main/java/com/example/starwarsapp/ui/student/StudentAdapter.kt
+++ app/src/main/java/com/example/starwarsapp/ui/student/StudentAdapter.kt
@@ -1,6 +1,10 @@
 package com.example.starwarsapp.ui.student
 
+
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+//import androidx.recyclerview.widget.RecyclerView
 import com.example.starwarsapp.logic.StudentEntity
 
 class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>(){
@@ -10,5 +14,10 @@ class StudentAdapter : RecyclerView.Adapter<StudentAdapter.MyViewHolder>(){
             field = value
             notifyDataSetChanged()
         }
+    override fun getItemCount() = students.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentAdapter.MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.brewery_item, parent, false))
+    }
 
 }
